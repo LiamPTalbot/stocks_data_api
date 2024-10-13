07a673f02e48172a1f0234c12500599b06b98441
LiamPTalbot <liam.p.talbot@gmail.com>
2024-10-13 13:18:12 +0100

    gaveup

--- server.js
+++ server.js
@@ -5,39 +5,19 @@ const cheerio = require('cheerio')
 let stockTicker = 'pypl'
 let type = 'history'
 
-async function scrapeData(ticker) {
+async function scrapeData() {
     try {
         // step a - fetch the page html
-        const url = `https://finance.yahoo.com/quote/${ticker}/${type}?p=${ticker}`
+        const url = `https://finance.yahoo.com/quote/${stockTicker}/${type}?p=${stockTicker}`
         const res = await fetch(url)
         const html = await res.text()
 
         const $ = cheerio.load(html)
         const price_history = getPrices($)
-        return price_history
         console.log(price_history)
     } catch (err) {
         console.log(err.message)
     }
-}
-
-async function fetchHTML(ticker, type) {
-    const url = `https://finance.yahoo.com/quote/${ticker}/${type}?p=${ticker}`
-    let headers = {
-        "Content-Type": "application/json",
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Referer': 'https://www.google.com/',
-        'Connection': 'keep-alive',
-    }
-    const res = await fetch(url, {
-        method: 'GET',
-        headers: headers
-    })
-    const html = await res.text()
-    return html
-    const $ = cheerio.load(html)
 }
 
 function getPrices(cher) {
@@ -46,6 +26,8 @@ function getPrices(cher) {
     })
     return prices
 }
+
+scrapeData()
 
 // step 2 - initialize server that serves up an html file that the user can play with
 
